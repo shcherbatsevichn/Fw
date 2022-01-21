5bbd5ce36814fd76130811db3a20191337b5ca12
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-21 11:31:09 +0300

    stage 1.02

--- Core/Application.php
+++ Core/Application.php
@@ -8,11 +8,11 @@ class Application{
     private static $instance = null;
     private $template = null;
 
-    protected function __construct(){  //предотвращаем создание объекта через оператор new
+    private function __construct(){  //предотвращаем создание объекта через оператор new
     }
-    protected function __clone(){ //защита от клонирования
+    private function __clone(){ //защита от клонирования
     }
-    protected function __wakeup(){ // защита от восстановления из строки
+    private function __wakeup(){ // защита от восстановления из строки
     }
 
     public static function getInstance(): Application { 
