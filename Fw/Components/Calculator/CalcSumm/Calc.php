f29cd2435a8aa71b0fcff0044e0f2b4eab855a60
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-02-03 14:57:25 +0300

    stage-3-2

--- Fw/Components/Calculator/CalcSumm/Calc.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Fw\Components\Calculator\CalcSumm;
-
-class Calc{  //логика вычисеней компонента вынесена в отдельный класс
-
-    public function getSumm($a, $b){
-    return $a + $b; 
-    }
-}
